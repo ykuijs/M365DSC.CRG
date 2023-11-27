c392be4be7e242dfaf23a04101afc4850206402e
avarga77 <149158215+avarga77@users.noreply.github.com>
2023-11-27 17:58:51 +0100

    Update New-CompositeResourceModule.ps1
    Changed AAD resource name to AzureAD

--- source/Public/New-CompositeResourceModule.ps1
+++ source/Public/New-CompositeResourceModule.ps1
@@ -66,7 +66,7 @@ function New-CompositeResourceModule
                 }
                 AppCredentials = @(
                     @{
-                        Workload       = 'AAD'
+                        Workload       = 'AzureAD'
                         ApplicationId  = '878459e4-79e4-4f9a-83d6-738c32ddd5c2'
                         CertThumbprint = '65E427769F27CDA198231B2A7FF03940897FB687'
                     }
@@ -177,8 +177,8 @@ function New-CompositeResourceModule
                 {
                     { $_.StartsWith('AAD') }
                     {
-                        $resourceWorkload = 'AAD'
-                        $customResourceName = $shortResourceName -replace "^$resourceWorkload"
+                        $resourceWorkload = 'AzureAD'
+                        $customResourceName = $shortResourceName -replace "^AAD"
                     }
                     { $_.StartsWith('EXO') }
                     {
