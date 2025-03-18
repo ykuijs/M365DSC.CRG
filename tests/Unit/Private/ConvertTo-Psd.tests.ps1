8a69aa3dc11a71b806becd7dbd0fc5945f8fd674
Yorick Kuijs <ykuijs@hotmail.com>
2025-03-18 13:57:04 +0100

    Corrected test

--- tests/Unit/Private/ConvertTo-Psd.tests.ps1
+++ tests/Unit/Private/ConvertTo-Psd.tests.ps1
@@ -14,15 +14,15 @@ Describe ConvertTo-Psd {
         It 'Should return a string representation of the hashtable' {
             InModuleScope -ModuleName $dscModuleName {
                 $configData = [ordered]@{
-                    Value1 = "String1"
+                    Value1 = 'String1'
                     Value2 = 25
                     Value3 = [ordered]@{
-                        Value4 = "String2"
+                        Value4 = 'String2'
                         Value5 = 50
                     }
                     Value6 = @(
                         [ordered]@{
-                            Value7 = "String3"
+                            Value7   = 'String3'
                             Value8 = 75
                         }
                     )
@@ -40,9 +40,16 @@ Describe ConvertTo-Psd {
                 [void]$compareString.AppendLine("        @{")
                 [void]$compareString.AppendLine("            Value7 = 'String3'")
                 [void]$compareString.AppendLine("            Value8 = 75")
+                [void]$compareString.AppendLine("            Value9 = '67e38349-8278-4968-a77e-81f810d33172'")
+                [void]$compareString.AppendLine("            Value10 = 'www.contoso.com'")
+                [void]$compareString.AppendLine("            Value_11 = 'String4'")
+                [void]$compareString.AppendLine("            Value12 = 100")
+                [void]$compareString.AppendLine("            Value13 = 1073741824")
                 [void]$compareString.AppendLine("        }")
                 [void]$compareString.AppendLine("    )")
-                [void]$compareString.Append("}")
+                [void]$compareString.AppendLine("    Value20 = [DateTime] '2024-01-01T00:00:00.0000000'")
+                [void]$compareString.AppendLine("    Value21 = `$true")
+                [void]$compareString.AppendLine("}")
 
                 ConvertTo-Psd -InputObject $configData | Should -Be $compareString.ToString()
             }
